55c78004d0b72048f0e963493a70f37cb46644bc
KaelenB <ktbrown@ualberta.ca>
2023-05-22 12:35:30 -0600

    init

--- config/file-upload-config.js
+++ config/file-upload-config.js
@@ -1,10 +1,10 @@
 const multer = require('multer');
 const fs = require('fs');
 
-function fileFilter (req, file, callback) {
-  var errorMessage = '';
+const fileFilter = (req, file, callback) => {
+  const errorMessage = '';
   if (!file || file.mimetype !== 'video/mp4') {
-    errorMessage = 'Wrong file type \"' + file.originalname.split('.').pop() + '\" found. Only mp4 video files are allowed!';
+    errorMessage = `Wrong file type \"${file.originalname.split('.').pop()}\" found. Only mp4 video files are allowed!`;
   }
   if(errorMessage) {
     return callback({errorMessage: errorMessage, code: 'LIMIT_FILE_TYPE'}, false);
@@ -12,8 +12,8 @@ function fileFilter (req, file, callback) {
   callback(null, true);
 }
 
-function destinationPath(req, file, callback) {
-  var stat = null;
+const destinationPath = (req, file, callback) => {
+  let stat = null;
   try {
     stat = fs.statSync(process.env.FILE_UPLOAD_PATH);
   } catch (err) {
@@ -22,14 +22,13 @@ function destinationPath(req, file, callback) {
    callback(null, process.env.FILE_UPLOAD_PATH);
 }
 
-function fileNameConvention(req, file, callback) {
+const fileNameConvention = (req, file, callback) => {
   callback(null, Date.now() + '-' + file.originalname.replace(/ /g, '_'));
 }
 
 const limits = {
   fileSize: parseInt(process.env.FILE_SIZE) * 1024 * 1024 // 200MB
 }
-
 
 const storage = multer.diskStorage({
   destination: destinationPath,
--- controllers/video-upload-controller.js
+++ controllers/video-upload-controller.js
@@ -3,26 +3,26 @@ const fileUploadConfig = require('../config/file-upload-config').fileUploadConfi
 const handleDb = require('../db/handle-db');
 const multer  = require('multer');
 
-module.exports.initUploadPage = function(req, res) {
+module.exports.initUploadPage = (req, res) => {
   res.sendFile(path.resolve(__dirname + '/../public/video_upload_test.html'));
 }
 
-module.exports.uploadFile = function(req, res) {
-  var upload = multer(fileUploadConfig).single('user-file');
+module.exports.uploadFile = (req, res) => {
+  const upload = multer(fileUploadConfig).single('user-file');
   upload(req, res, function(uploadError){
     if(uploadError){
-      var errorMessage;
+      let errorMessage;
       if(uploadError.code === 'LIMIT_FILE_TYPE') {
         errorMessage = uploadError.errorMessage;
       } else if(uploadError.code === 'LIMIT_FILE_SIZE'){
-          errorMessage = 'Maximum file size allowed is ' + process.env.FILE_SIZE + 'MB';
+          errorMessage = `Maximum file size allowed is ${process.env.FILE_SIZE}MB`;
       }
       return res.json({
         error: errorMessage
       });
     }
     const fileId = req.file.filename.split('-')[0];
-    const link = 'http://' + req.hostname + ':' + process.env.PORT + '/video/' + fileId
+    const link = `http://${req.hostname}:${process.env.PORT}/video/${req.file.filename}`;
 
     res.json({
       success: true,
@@ -37,6 +37,5 @@ module.exports.uploadFile = function(req, res) {
       details: req.body.details ? req.body.details : ''
     }
     handleDb.saveToDB(attributesToBeSaved);
-    // return res.send(req.file);
   });
 }
