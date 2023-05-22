6d930b1bbcc825208195194d4d700fdb202296fa
KaelenB <ktbrown@ualberta.ca>
2023-05-22 12:16:28 -0600

    Revert "refactor"
    This reverts commit 45b007297b04df0ae5bc4ebf26ce46d90bc13f52.

--- config/file-upload-config.js
+++ config/file-upload-config.js
@@ -1,50 +1,45 @@
-import { diskStorage } from "multer";
-import { statSync, mkdirSync } from "fs";
+const multer = require('multer');
+const fs = require('fs');
 
-const fileFilter = (req, file, callback) => {
-  const errorMessage = "";
-  if (!file || file.mimetype !== "video/mp4") {
-    errorMessage = `Wrong file type ${file.originalname
-      .split(".")
-      .pop()}found. Only mp4 video files are allowed!`;
+function fileFilter (req, file, callback) {
+  var errorMessage = '';
+  if (!file || file.mimetype !== 'video/mp4') {
+    errorMessage = 'Wrong file type \"' + file.originalname.split('.').pop() + '\" found. Only mp4 video files are allowed!';
   }
-  if (errorMessage) {
-    return callback(
-      { errorMessage: errorMessage, code: "LIMIT_FILE_TYPE" },
-      false
-    );
+  if(errorMessage) {
+    return callback({errorMessage: errorMessage, code: 'LIMIT_FILE_TYPE'}, false);
   }
   callback(null, true);
-};
+}
 
-const destinationPath = (req, file, callback) => {
-  const stat = null;
+function destinationPath(req, file, callback) {
+  var stat = null;
   try {
-    stat = statSync(process.env.FILE_UPLOAD_PATH);
+    stat = fs.statSync(process.env.FILE_UPLOAD_PATH);
   } catch (err) {
-    mkdirSync(process.env.FILE_UPLOAD_PATH);
+    fs.mkdirSync(process.env.FILE_UPLOAD_PATH);
   }
-  callback(null, process.env.FILE_UPLOAD_PATH);
-};
+   callback(null, process.env.FILE_UPLOAD_PATH);
+}
 
-const fileNameConvention = (req, file, callback) => {
-  callback(null, Date.now() + "-" + file.originalname.replace(/ /g, "_"));
-};
+function fileNameConvention(req, file, callback) {
+  callback(null, Date.now() + '-' + file.originalname.replace(/ /g, '_'));
+}
 
 const limits = {
-  fileSize: parseInt(process.env.FILE_SIZE) * 1024 * 1024, // 200MB
-};
+  fileSize: parseInt(process.env.FILE_SIZE) * 1024 * 1024 // 200MB
+}
 
-const storage = diskStorage({
+
+const storage = multer.diskStorage({
   destination: destinationPath,
-  filename: fileNameConvention,
+  filename: fileNameConvention
 });
 
 const fileUploadConfig = {
   fileFilter: fileFilter,
   storage: storage,
-  limits: limits,
+  limits: limits
 };
 
-const _fileUploadConfig = fileUploadConfig;
-export { _fileUploadConfig as fileUploadConfig };
+module.exports.fileUploadConfig = fileUploadConfig;
